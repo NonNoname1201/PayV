d7b230182033e6d9aa934765b3715acb28b06d31
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-08-18 12:27:23 +0200

    Creation of payment structure

---
+++ PaymentData.cs
@@ -0,0 +1,17 @@
+﻿namespace PayV;
+
+public struct PaymentData(
+    string paymentStatus,
+    string paymentMethod,
+    string paymentCurrency,
+    float paymentAmount,
+    string from,
+    string to)
+{
+    private string PaymentStatus { get; set; } = paymentStatus;
+    private string PaymentMethod { get; set; } = paymentMethod;
+    private string PaymentCurrency { get; set; } = paymentCurrency;
+    private float PaymentAmount { get; set; } = paymentAmount;
+    private string From { get; set; } = from;
+    private string To { get; set; } = to;
+}
