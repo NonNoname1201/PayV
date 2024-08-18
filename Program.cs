b11a8455f08cb8eecbab2c9a6d2132e5f0d8d1ce
NonNoname1201 <dobrohorskyi.v@gmail.com>
2024-08-18 12:49:17 +0200

    Implementation of an expiration service

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<Dictionary<string, PaymentData>>();
+builder.Services.AddHostedService<ExpirationService>();
 
 var app = builder.Build();
 
@@ -18,25 +20,28 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var temporaryLinks = new Dictionary<string, PaymentData>() { };
+var temporaryLinks = new Dictionary<string, PaymentData>();
 
 app.MapPost("/getlink", (string from, string to, float amount) =>
 {
     var link = Guid.NewGuid().ToString() + DateTime.UtcNow.Ticks.ToString();
     temporaryLinks.Add(link, new PaymentData
     (
-        "Pending",   // PaymentStatus
+        "Pending", // PaymentStatus
         "undefined", // PaymentMethod
         "undefined", // PaymentCurrency
         amount,
         from,
-        to
+        to,
+        DateTime.UtcNow.AddMinutes(5) // ExpirationTime
     ));
     return link;
 }).WithName("GetLink").WithOpenApi();
 
-app.MapPost("/payment/{id}", (string id, bool approval) => 
-    approval ? $"Payment with id {id} was successful" : $"Payment with id {id} was declined")
+app.MapPost("/payment/{id}", (string id, bool approval) =>
+        temporaryLinks.ContainsKey(id)
+            ? (approval ? $"Payment with id {id} was successful" : $"Payment with id {id} was declined")
+            : "Payment link does not exists or was expired")
     .WithName("FinishPayment").WithOpenApi();
 
 app.Run();
